77403a52c7a471c4304247a358e6a91ef712cc29
Michael Gillund <79627666+michaelgillund@users.noreply.github.com>
2022-11-02 01:20:59 -0500

    Initial Commit

---
+++ Array/ArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArrayApp.swift
+//  Array
+//
+//  Created by Michael Gillund on 11/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
